01f5231d1877f253044a39b7466f90a858af4613
Safa Ben Othmen <safabenothmen26@gmail.com>
2025-05-09 19:12:18 +0200

    supprimer backend

--- backend/src/main/java/tn/OperationsMaintenance/repository/InterventionRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.OperationsMaintenance.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tn.OperationsMaintenance.entity.Intervention;
-
-public interface InterventionRepository extends JpaRepository<Intervention, Integer> {
-
-}
